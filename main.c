0922f235e8633ad2d5c0f83477305fa5865581e9
LuisRayas <luis.reyes7482@alumnos.udg.mx>
2020-09-23 00:50:26 +0000

    Preguntamos si el usuario quiere -/+ 2 numeros

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
+#include "funciones.h"
+
 int main(){
     char nombre[100];
     unsigned int edad;
+    int num1 , num2, opc;
 
     printf("Hola mundo\n");
     printf("Ingrese su nombre: \n");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s\n", nombre);
-    printf("Edad: ");
+    printf("Edad: \n");
     scanf("%u", &edad);
-    printf("Vas a cumplir %u anios", edad + 1);
+    printf("Vas a cumplir %u anios \n", edad + 1);
+    printf("Introduce un numero: \n");
+    scanf("%d", &num1);
+    printf("Introduce otro numero: \n");
+    scanf("%d", &num2);
+    printf("1. Sumar numeros \n 2. Restar numeros.\n");
+    scanf("%d", &opc);
+    if(opc == 1)
+      Sumar(num1, num2);
+    else
+      Restar(num1, num2);    
+
     return 0;
 }
